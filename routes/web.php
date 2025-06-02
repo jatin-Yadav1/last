16506a155216a5f5cd1c38eca25028e2704697e7
tylatwistdev <official.tylatwist@gmail.com>
2025-06-02 21:42:04 +0530

    update coming-soon

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+     return view('coming-soon');
 });
 
-Route::get('/coming-soon', function () {
-    return view('coming-soon');
-});
+// Route::get('/coming-soon', function () {
+//     return view('coming-soon');
+// });
